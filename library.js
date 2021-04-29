78ebd7f36820dff6175814b8bc2305a126449b9d
Daniel Kozminski <danielkozminski@hotmail.com>
2021-04-28 20:32:11 -0400

    version 1 working, no local or could storage

--- library.js
+++ library.js
@@ -7,7 +7,7 @@ function Book(title, author, pages, read) {
 }
 
 Book.prototype.info = function () {
-    return `${this.title}\n\n By: ${this.author}\n ${this.pages} pages\n ${this.read}`;
+    return `${this.title}\n\n By: ${this.author}\n ${this.pages} pages`;
 }
 
 document.getElementById('submit').addEventListener('click', function () {
@@ -29,11 +29,10 @@ document.getElementById('cancel').addEventListener('click', function () {
     document.getElementById('newBook').style.display = 'block'
 })
 
-document.getElementById("form").style.display = 'none';
+document.getElementById('form').style.display = 'none';
 document.getElementById('newBook').addEventListener('click', function () {
     document.getElementById('form').style.display = 'block';
     document.getElementById('newBook').style.display = 'none'
-
 })
 
 function renderLibrary() {
@@ -42,22 +41,50 @@ function renderLibrary() {
         let newDiv = document.createElement('div');
         newDiv.className = 'book';
         newDiv.id = `book${i}`;
+        newDiv.setAttribute('data-arrIndex', `${i}`);
         newDiv.innerText = `${myLibrary[i].info()}`
         document.getElementById('library').appendChild(newDiv);
 
         document.getElementById(`book${i}`).innerHTML +=
-            ` <input type = 'button' id = 'readButton${i}' value = 'Read?'>`
+            `<div class='radioButtons'>
+                <input type='radio' id='read${i}' name='read${i}' value='Have Read'>
+                <label for='read${i}'>Have Read</label>
+                <input type='radio' id='notRead${i}' name='read${i}' value='Have not Read'>
+                <label for='notRead${i}'>Have Not Read</label>
+            </div>
+            <input type='button' class='delete' id='delete${i}' value='X'>`;
+
+        if (myLibrary[i].read == 'Have Read') {
+            document.getElementById(`read${i}`).checked = true;
+        } else { document.getElementById(`notRead${i}`).checked = true };
+
+        document.getElementById(`read${i}`).addEventListener('change', function () {
+            myLibrary[this.parentElement.parentElement.getAttribute('data-arrIndex')].read = 'Have Read'
+        })
+
+        document.getElementById(`notRead${i}`).addEventListener('change', function () {
+            myLibrary[this.parentElement.parentElement.getAttribute('data-arrIndex')].read = 'Have not Read'
+        })
+
+        document.getElementById(`delete${i}`).addEventListener('click', function () {
+            myLibrary.splice(this.parentElement.parentElement.getAttribute('data-arrIndex'), 1)
+            renderLibrary();
+        })
+
     }
 }
 
+myLibrary.push(new Book('Version 1.0', '(CSS needs work)', '(no memory)'))
 myLibrary.push(new Book('The Hobbit', 'Tolkien', '495', 'Have Read'))
 myLibrary.push(new Book('The Cat in the Hat', 'Dr. Suess', '49', 'Have not Read'))
-myLibrary.push(new Book('Of Mice and Men', 'somebody', '205', 'Have Read'))
+myLibrary.push(new Book('Of Mice and Men', 'John Steinbeck', '205', 'Have Read'))
 renderLibrary();
 
 
 
-// toggle read status button
+
+
+
 
 
 
